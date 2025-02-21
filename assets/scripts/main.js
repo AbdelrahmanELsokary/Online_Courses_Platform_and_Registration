7c07255f70476ca15173bc982a97defe6dab12ce
Abdelrahman Elsokary <abdalrahmanelsokarey@gmail.com>
2025-02-22 00:47:07 +0200

    Edit: Src Icons

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,11 @@
 document.getElementById('toggel').onclick = () => {
   document.getElementById('menu').classList.toggle('menu_open');
 };
+
+// document.getElementById('dark_mode').onclick = () => {
+//   if (document.body.classList.contains('dark_mode')) {
+//     document.body.classList.remove('dark_mode');
+//   } else {
+//     document.body.classList.add('dark_mode')
+//   }
+// };
